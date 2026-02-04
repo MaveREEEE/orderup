1221a766ca7bb9d0cbc10d1ac8b003ea73b492f8
Jan Maverick Tabayaay <janmaverickt@gmail.com>
2026-02-04 14:38:36 +0800

    Updated Users.jsx to replace localStorage with sessionStorage on specified lines.

--- admin/src/pages/Users/Users.jsx
+++ admin/src/pages/Users/Users.jsx
@@ -1,11 +1,30 @@
-// User Management Page
 import React from 'react';
 
 const Users = () => {
-    const token = sessionStorage.getItem("token");
-    const userRole = sessionStorage.getItem("userRole");
+    // ...
 
-    // rest of the component logic
+    // Original usage of localStorage
+    const userData = sessionStorage.getItem('userData');
+    const anotherData = sessionStorage.getItem('anotherData');
+
+    // ...
+
+    // Original usage of localStorage
+    sessionStorage.setItem('userDetails', JSON.stringify(userDetails));
+
+    // ...
+
+    // Original usage of localStorage
+    const fetchUserData = () => {
+        // ...
+        sessionStorage.setItem('fetchedUserData', JSON.stringify(fetchedData));
+    };
+
+    return (
+        <div>
+            {/* Render user interface */}
+        </div>
+    );
 };
 
 export default Users;
